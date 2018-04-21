0e454990c737c5c6c06c0dedd8d06287a083a01a
zhouchenhua <noatnu@163.com>
2018-04-21 18:28:05 +0800

    指数 提交

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/entity/HousePriceIndex.java
@@ -0,0 +1,90 @@
+package com.copower.pmcc.assess.dal.entity;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class HousePriceIndex {
+    public static String INDEX_CALENDAR_KEY = "indexCalendar";
+    private Integer id;
+
+    @DateTimeFormat(pattern = "yyyy-MM")
+    private Date yearMonthCalendar;
+    private String  yearMonthString;
+
+    private String indexCalendar;
+
+    private String creator;
+
+    private Date gmtCreated;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Date getYearMonthCalendar() {
+        return yearMonthCalendar;
+    }
+
+    public void setYearMonthCalendar(Date yearMonthCalendar) {
+        this.yearMonthCalendar = yearMonthCalendar;
+    }
+
+    public String getIndexCalendar() {
+        return indexCalendar;
+    }
+
+    public void setIndexCalendar(String indexCalendar) {
+        this.indexCalendar = indexCalendar == null ? null : indexCalendar.trim();
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    @Override
+    public String toString() {
+        return "HousePriceIndex{" +
+                "id=" + id +
+                ", yearMonthCalendar=" + yearMonthCalendar +
+                ", indexCalendar='" + indexCalendar + '\'' +
+                ", creator='" + creator + '\'' +
+                ", gmtCreated=" + gmtCreated +
+                ", gmtModified=" + gmtModified +
+                '}';
+    }
+
+    public String getYearMonthString() {
+        return yearMonthString;
+    }
+
+    public void setYearMonthString(String yearMonthString) {
+        this.yearMonthString = yearMonthString;
+    }
+}
---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/mapper/HousePriceIndexMapper.java
@@ -0,0 +1,30 @@
+package com.copower.pmcc.assess.dal.mapper;
+
+import com.copower.pmcc.assess.dal.entity.HousePriceIndex;
+import com.copower.pmcc.assess.dal.entity.HousePriceIndexExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface HousePriceIndexMapper {
+    int countByExample(HousePriceIndexExample example);
+
+    int deleteByExample(HousePriceIndexExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(HousePriceIndex record);
+
+    int insertSelective(HousePriceIndex record);
+
+    List<HousePriceIndex> selectByExample(HousePriceIndexExample example);
+
+    HousePriceIndex selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") HousePriceIndex record, @Param("example") HousePriceIndexExample example);
+
+    int updateByExample(@Param("record") HousePriceIndex record, @Param("example") HousePriceIndexExample example);
+
+    int updateByPrimaryKeySelective(HousePriceIndex record);
+
+    int updateByPrimaryKey(HousePriceIndex record);
+}
