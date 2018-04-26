7dcb1cd4e4a5962d4e7b17c4c7783bdc422ec5c7
zhouchenhua <noatnu@163.com>
2018-04-26 17:50:14 +0800

    评估方法第四次修改

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/mapper/EvaluationThinkingFieldMapper.java
@@ -0,0 +1,30 @@
+package com.copower.pmcc.assess.dal.mapper;
+
+import com.copower.pmcc.assess.dal.entity.EvaluationThinkingField;
+import com.copower.pmcc.assess.dal.entity.EvaluationThinkingFieldExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EvaluationThinkingFieldMapper {
+    int countByExample(EvaluationThinkingFieldExample example);
+
+    int deleteByExample(EvaluationThinkingFieldExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(EvaluationThinkingField record);
+
+    int insertSelective(EvaluationThinkingField record);
+
+    List<EvaluationThinkingField> selectByExample(EvaluationThinkingFieldExample example);
+
+    EvaluationThinkingField selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") EvaluationThinkingField record, @Param("example") EvaluationThinkingFieldExample example);
+
+    int updateByExample(@Param("record") EvaluationThinkingField record, @Param("example") EvaluationThinkingFieldExample example);
+
+    int updateByPrimaryKeySelective(EvaluationThinkingField record);
+
+    int updateByPrimaryKey(EvaluationThinkingField record);
+}
