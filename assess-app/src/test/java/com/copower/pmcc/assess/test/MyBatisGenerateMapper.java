678fd64e326ed9d496e2aa949044e49cf577f11a
zhouchenhua <noatnu@163.com>
2018-05-15 14:15:14 +0800

    2018-05-15 项目立项修改 第5次提交

--- assess-app/src/test/java/com/copower/pmcc/assess/test/MyBatisGenerateMapper.java
+++ assess-app/src/test/java/com/copower/pmcc/assess/test/MyBatisGenerateMapper.java
@@ -22,11 +22,12 @@ import java.util.List;
  * @date: 2017/09/14 17:08
  */
 public class MyBatisGenerateMapper {
+    private static String fileName = "zchgeneratorConfig" +".xml";
 
     public static void main(String[] args) throws Exception {
 
         List<File> files = new ArrayList();
-        findFiles(System.getProperty("user.dir"), "generatorConfig.xml", files);
+        findFiles(System.getProperty("user.dir"), fileName, files);
         if (CollectionUtils.isNotEmpty(files)) {
             for (File configFile: files) {
 
