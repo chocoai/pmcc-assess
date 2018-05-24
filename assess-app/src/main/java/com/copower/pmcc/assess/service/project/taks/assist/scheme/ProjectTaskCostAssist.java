5b1f0db3512a87bfd324c8c08d2949384fbaf6a6
wangpc <372083418@qq.com>
2018-05-24 11:53:16 +0800

    添加方案提交成果对应文件

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/project/taks/assist/scheme/ProjectTaskCostAssist.java
@@ -0,0 +1,68 @@
+package com.copower.pmcc.assess.service.project.taks.assist.scheme;
+
+import com.copower.pmcc.assess.controller.ControllerComponent;
+import com.copower.pmcc.assess.dal.entity.ProjectPlanDetails;
+import com.copower.pmcc.assess.proxy.face.ProjectTaskInterface;
+import com.copower.pmcc.bpm.api.annotation.WorkFlowAnnotation;
+import com.copower.pmcc.erp.common.exception.BusinessException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 描述:
+ *
+ * @author: Calvin(qiudong@copowercpa.com)
+ * @data: 2018/1/30
+ * @time: 14:15
+ */
+@Component
+@WorkFlowAnnotation(desc = "成本法成果")
+public class ProjectTaskCostAssist implements ProjectTaskInterface {
+    @Autowired
+    private ControllerComponent serviceComponent;
+
+    @Override
+    public ModelAndView applyView(ProjectPlanDetails projectPlanDetails) {
+        ModelAndView modelAndView = serviceComponent.baseFormModelAndView("/task/scheme/taskCostIndex", "", 0, "0", "");
+        return modelAndView;
+    }
+
+    @Override
+    public ModelAndView approvalView(String processInsId, String taskId, Integer boxId, ProjectPlanDetails projectPlanDetails, String agentUserAccount) {
+        ModelAndView modelAndView = serviceComponent.baseFormModelAndView("/task/scheme/taskCostApproval", processInsId, boxId, taskId, agentUserAccount);
+        return modelAndView;
+    }
+
+    @Override
+    public ModelAndView returnEditView(String processInsId, String taskId, Integer boxId, ProjectPlanDetails projectPlanDetails, String agentUserAccount) {
+        ModelAndView modelAndView = serviceComponent.baseFormModelAndView("/task/scheme/taskCostIndex", processInsId, boxId, taskId, agentUserAccount);
+        return modelAndView;
+    }
+
+    @Override
+    public void saveDraft(ProjectPlanDetails projectPlanDetails, String formData) throws BusinessException {
+
+    }
+
+    @Override
+    public ModelAndView detailsView(ProjectPlanDetails projectPlanDetails,Integer boxId){
+        ModelAndView modelAndView = serviceComponent.baseFormModelAndView("/task/scheme/taskCostApproval", projectPlanDetails.getProcessInsId(), boxId, "-1", "");
+        return modelAndView;
+    }
+
+    @Override
+    public void applyCommit(ProjectPlanDetails projectPlanDetails, String processInsId, String formData) throws BusinessException {
+
+    }
+
+    @Override
+    public void approvalCommit(ProjectPlanDetails projectPlanDetails, String processInsId, String formData) throws BusinessException {
+
+    }
+
+    @Override
+    public void returnEditCommit(ProjectPlanDetails projectPlanDetails, String processInsId, String formData) throws BusinessException {
+
+    }
+}
