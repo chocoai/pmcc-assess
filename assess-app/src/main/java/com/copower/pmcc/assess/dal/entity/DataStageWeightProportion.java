a6cea4f4e8842e2515c1af3dbfa5e50fdb6c1309
wangpc <372083418@qq.com>
2018-07-02 16:45:06 +0800

    阶段权重配置调整，项目立项区域选择控件完善，建筑成新率配置bug调整，立项对应枚举数据调整

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/entity/DataStageWeightProportion.java
@@ -0,0 +1,75 @@
+package com.copower.pmcc.assess.dal.entity;
+
+import java.util.Date;
+
+public class DataStageWeightProportion {
+    private Integer id;
+
+    private Integer entrustPurpose;
+
+    private Integer stage;
+
+    private Integer proportion;
+
+    private String creator;
+
+    private Date gmtCreated;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getEntrustPurpose() {
+        return entrustPurpose;
+    }
+
+    public void setEntrustPurpose(Integer entrustPurpose) {
+        this.entrustPurpose = entrustPurpose;
+    }
+
+    public Integer getStage() {
+        return stage;
+    }
+
+    public void setStage(Integer stage) {
+        this.stage = stage;
+    }
+
+    public Integer getProportion() {
+        return proportion;
+    }
+
+    public void setProportion(Integer proportion) {
+        this.proportion = proportion;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
