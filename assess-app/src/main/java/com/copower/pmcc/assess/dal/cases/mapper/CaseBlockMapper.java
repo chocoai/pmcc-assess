095b69cca66f14e01aec8b04913c1d018706d28a
wangpc <372083418@qq.com>
2018-07-05 15:26:23 +0800

    代码找回

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/cases/mapper/CaseBlockMapper.java
@@ -0,0 +1,30 @@
+package com.copower.pmcc.assess.dal.cases.mapper;
+
+import com.copower.pmcc.assess.dal.cases.entity.CaseBlock;
+import com.copower.pmcc.assess.dal.cases.entity.CaseBlockExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface CaseBlockMapper {
+    int countByExample(CaseBlockExample example);
+
+    int deleteByExample(CaseBlockExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(CaseBlock record);
+
+    int insertSelective(CaseBlock record);
+
+    List<CaseBlock> selectByExample(CaseBlockExample example);
+
+    CaseBlock selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") CaseBlock record, @Param("example") CaseBlockExample example);
+
+    int updateByExample(@Param("record") CaseBlock record, @Param("example") CaseBlockExample example);
+
+    int updateByPrimaryKeySelective(CaseBlock record);
+
+    int updateByPrimaryKey(CaseBlock record);
+}
