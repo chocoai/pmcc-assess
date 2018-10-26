80dee161fef1c64b0aa42a2234c5676cce8e39f8
wangpc <372083418@qq.com>
2018-10-26 10:47:57 +0800

    方案提交主表更新methtype，确定单价建立从表数据

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/basis/entity/SchemeSurePriceItem.java
@@ -0,0 +1,96 @@
+package com.copower.pmcc.assess.dal.basis.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class SchemeSurePriceItem {
+    private Integer id;
+
+    private Integer judgeObjectId;
+
+    private String methodName;
+
+    private BigDecimal trialPrice;
+
+    private BigDecimal weight;
+
+    private BigDecimal errorRate;
+
+    private String creator;
+
+    private Date gmtCreated;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getJudgeObjectId() {
+        return judgeObjectId;
+    }
+
+    public void setJudgeObjectId(Integer judgeObjectId) {
+        this.judgeObjectId = judgeObjectId;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName == null ? null : methodName.trim();
+    }
+
+    public BigDecimal getTrialPrice() {
+        return trialPrice;
+    }
+
+    public void setTrialPrice(BigDecimal trialPrice) {
+        this.trialPrice = trialPrice;
+    }
+
+    public BigDecimal getWeight() {
+        return weight;
+    }
+
+    public void setWeight(BigDecimal weight) {
+        this.weight = weight;
+    }
+
+    public BigDecimal getErrorRate() {
+        return errorRate;
+    }
+
+    public void setErrorRate(BigDecimal errorRate) {
+        this.errorRate = errorRate;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
