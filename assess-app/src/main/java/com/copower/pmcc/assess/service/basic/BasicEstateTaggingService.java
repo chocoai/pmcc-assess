3c14fba56559ce4be7bde775b2b94b57966d5f44
wangpc <372083418@qq.com>
2018-12-04 18:35:00 +0800

    设置楼盘的标注

--- assess-app/src/main/java/com/copower/pmcc/assess/service/basic/BasicEstateTaggingService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/basic/BasicEstateTaggingService.java
@@ -2,15 +2,11 @@ package com.copower.pmcc.assess.service.basic;
 
 import com.copower.pmcc.assess.dal.basic.dao.BasicEstateTaggingDao;
 import com.copower.pmcc.assess.dal.basic.entity.BasicEstateTagging;
-import com.copower.pmcc.erp.api.dto.model.BootstrapTableVo;
 import com.copower.pmcc.erp.common.CommonService;
-import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
-import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -36,30 +32,6 @@ public class BasicEstateTaggingService {
      */
     public BasicEstateTagging getBasicEstateTaggingById(Integer id) throws Exception {
         return basicEstateTaggingDao.getBasicEstateTaggingById(id);
-    }
-
-
-    /**
-     * 新增或者修改
-     *
-     * @param basicEstateTagging
-     * @return
-     * @throws Exception
-     */
-    public void saveBasicEstateTagging(BasicEstateTagging basicEstateTagging) throws Exception {
-        StringBuilder stringBuilder = new StringBuilder();
-        if(!StringUtils.isEmpty(basicEstateTagging.getBuildingNumber())){
-            stringBuilder.append(basicEstateTagging.getBuildingNumber()).append("栋");
-        }
-        if(!StringUtils.isEmpty(basicEstateTagging.getUnitNumber())){
-            stringBuilder.append(basicEstateTagging.getUnitNumber()).append("单元");
-        }
-        if(!StringUtils.isEmpty(basicEstateTagging.getRemark())){
-            stringBuilder.append(basicEstateTagging.getRemark());
-        }
-        basicEstateTagging.setContent(stringBuilder.toString());
-        basicEstateTagging.setCreator(commonService.thisUserAccount());
-        basicEstateTaggingDao.addBasicEstateTagging(basicEstateTagging);
     }
 
     public void addBasicEstateTagging(BasicEstateTagging basicEstateTagging) throws Exception {
@@ -92,20 +64,4 @@ public class BasicEstateTaggingService {
     public void removeBasicEstateTagging(BasicEstateTagging basicEstateTagging) throws Exception {
         basicEstateTaggingDao.removeBasicEstateTagging(basicEstateTagging);
     }
-
-    public BootstrapTableVo getBootstrapTableVo(Integer estateId) throws Exception {
-        BootstrapTableVo vo = new BootstrapTableVo();
-        BasicEstateTagging where = new BasicEstateTagging();
-        if (estateId <= 0) {
-            where.setEstateId(0);
-            where.setCreator(commonService.thisUserAccount());
-        } else {
-            where.setEstateId(estateId);
-        }
-        List<BasicEstateTagging> basicEstateTaggingList = basicEstateTaggingDao.basicEstateTaggingList(where);
-        vo.setTotal((long) basicEstateTaggingList.size());
-        vo.setRows(ObjectUtils.isEmpty(basicEstateTaggingList) ? Lists.newArrayList() : basicEstateTaggingList);
-        return vo;
-    }
-
 }
