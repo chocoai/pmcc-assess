2d2e9f6586f80156be0936a11f2fd0c5b5fab113
wangpc <372083418@qq.com>
2018-12-26 08:52:50 +0800

    房屋完损度主从表保存与显示

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/cases/entity/CaseHouseDamagedDegreeDetail.java
@@ -0,0 +1,106 @@
+package com.copower.pmcc.assess.dal.cases.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class CaseHouseDamagedDegreeDetail {
+    private Integer id;
+
+    private Integer houseId;
+
+    private Integer damagedDegreeId;
+
+    private Integer type;
+
+    private String entityCondition;
+
+    private String entityConditionContent;
+
+    private BigDecimal score;
+
+    private String creator;
+
+    private Date gmtCreated;
+
+    private Date gmtModified;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(Integer houseId) {
+        this.houseId = houseId;
+    }
+
+    public Integer getDamagedDegreeId() {
+        return damagedDegreeId;
+    }
+
+    public void setDamagedDegreeId(Integer damagedDegreeId) {
+        this.damagedDegreeId = damagedDegreeId;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
+    public String getEntityCondition() {
+        return entityCondition;
+    }
+
+    public void setEntityCondition(String entityCondition) {
+        this.entityCondition = entityCondition == null ? null : entityCondition.trim();
+    }
+
+    public String getEntityConditionContent() {
+        return entityConditionContent;
+    }
+
+    public void setEntityConditionContent(String entityConditionContent) {
+        this.entityConditionContent = entityConditionContent == null ? null : entityConditionContent.trim();
+    }
+
+    public BigDecimal getScore() {
+        return score;
+    }
+
+    public void setScore(BigDecimal score) {
+        this.score = score;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator == null ? null : creator.trim();
+    }
+
+    public Date getGmtCreated() {
+        return gmtCreated;
+    }
+
+    public void setGmtCreated(Date gmtCreated) {
+        this.gmtCreated = gmtCreated;
+    }
+
+    public Date getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Date gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+}
