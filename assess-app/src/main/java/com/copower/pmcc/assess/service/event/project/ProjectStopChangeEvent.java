8995c9443906b5f734522759956205d97317b938
huhao <1043712950@qq.com>
2019-02-19 14:38:05 +0800

    项目终止后关闭流程，删除待提交任务

--- assess-app/src/main/java/com/copower/pmcc/assess/service/event/project/ProjectStopChangeEvent.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/event/project/ProjectStopChangeEvent.java
@@ -9,6 +9,9 @@ import com.copower.pmcc.assess.service.project.ProjectInfoService;
 import com.copower.pmcc.assess.service.project.ProjectStateChangeService;
 import com.copower.pmcc.bpm.api.dto.model.ProcessExecution;
 import com.copower.pmcc.bpm.api.enums.ProcessStatusEnum;
+·import com.copower.pmcc.bpm.api.provider.BpmRpcActivitiProcessManageService;
+import com.copower.pmcc.bpm.api.provider.BpmRpcProjectTaskService;
+import com.copower.pmcc.erp.constant.ApplicationConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +24,13 @@ public class ProjectStopChangeEvent extends BaseProcessEvent {
     private ProjectStateChangeService simpleChangeService;
     @Autowired
     private ProjectChangeLogDao projectChangeLogDao;
+    @Autowired
+    private ApplicationConstant applicationConstant;
+    @Autowired
+    private BpmRpcProjectTaskService bpmRpcProjectTaskService;
+    @Autowired
+    private BpmRpcActivitiProcessManageService bpmRpcActivitiProcessManageService;
+
     @Override
     public void processFinishExecute(ProcessExecution processExecution) throws Exception {
         super.processFinishExecute(processExecution);
@@ -30,5 +40,9 @@ public class ProjectStopChangeEvent extends BaseProcessEvent {
         ProjectInfo projectInfo = projectInfoService.getProjectInfoById(data.getProjectId());
         projectInfo.setProjectStatus(ProjectStatusEnum.CLOSE.getKey());
         projectInfoService.saveProjectInfo(projectInfo);
+        //删除待提交任务
+        bpmRpcProjectTaskService.deleteProjectTaskByProjectid(applicationConstant.getAppKey(),projectInfo.getId());
+        //关闭流程
+        bpmRpcActivitiProcessManageService.closeProcess(projectInfo.getProcessInsId());
     }
 }
