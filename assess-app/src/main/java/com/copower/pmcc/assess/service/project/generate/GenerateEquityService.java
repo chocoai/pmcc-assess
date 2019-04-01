193041b23b72faae7c6d8f42ad0ae04c06622d48
wangpc <372083418@qq.com>
2019-04-01 11:03:11 +0800

    权益分析

--- assess-app/src/main/java/com/copower/pmcc/assess/service/project/generate/GenerateEquityService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/project/generate/GenerateEquityService.java
@@ -8,30 +8,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class GenerateEquityService {
 
-    /**
-     * 获取土地权益状况
-     * @return
-     */
-    public String getLandEquity(){
+    //1.土地权益状况
+    //权证权益
+    //规划条件
+    //土地利用状况
+    //他权类别
+    //他权特殊情况
 
-        return null;
-    }
 
-    /**
-     * 获取房屋权益状况
-     * @return
-     */
-    public String getHouseEquity(){
-
-        return null;
-    }
-
-    /**
-     * 获取他项权益状况
-     * @return
-     */
-    public String getOtherRightEquity(){
-
-        return null;
-    }
+    //2.房屋权益状况
+    //权证权益
+    //他权类别
+    //他权特殊情况
+    //物业服务
+    //物业公司
+    //房产评估价益综合评价
 }
