05d5e0a2e3c6b726667d66d4d15d35f76a4ed67e
wangpc <372083418@qq.com>
2019-07-08 14:32:40 +0800

    爬取案例自动任务

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/job/ClimbingCaseJob.java
@@ -0,0 +1,17 @@
+package com.copower.pmcc.assess.job;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by kings on 2019-7-8.
+ */
+@Component
+public class ClimbingCaseJob {
+
+    /**
+     * 爬取案例任务
+     */
+    public void climbing(){
+
+    }
+}
