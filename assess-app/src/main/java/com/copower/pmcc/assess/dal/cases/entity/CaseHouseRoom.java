1ced1ce5085a0d84fb2b14400281256cc2671798
wangpc <372083418@qq.com>
2019-07-23 19:05:14 +0800

    案例显示房间信息异常处理

--- assess-app/src/main/java/com/copower/pmcc/assess/dal/cases/entity/CaseHouseRoom.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/cases/entity/CaseHouseRoom.java
@@ -8,7 +8,7 @@ public class CaseHouseRoom {
 
     private Integer houseId;
 
-    private Integer roomType;
+    private String roomType;
 
     private String name;
 
@@ -56,12 +56,12 @@ public class CaseHouseRoom {
         this.houseId = houseId;
     }
 
-    public Integer getRoomType() {
+    public String getRoomType() {
         return roomType;
     }
 
-    public void setRoomType(Integer roomType) {
-        this.roomType = roomType;
+    public void setRoomType(String roomType) {
+        this.roomType = roomType == null ? null : roomType.trim();
     }
 
     public String getName() {
