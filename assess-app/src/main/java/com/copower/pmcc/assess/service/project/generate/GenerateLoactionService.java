52f6c9442744e33c83b286c7a0282bacb7769a2a
wangpc <372083418@qq.com>
2019-07-26 15:55:27 +0800

    merge

--- assess-app/src/main/java/com/copower/pmcc/assess/service/project/generate/GenerateLoactionService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/project/generate/GenerateLoactionService.java
@@ -127,8 +127,15 @@ public class GenerateLoactionService {
         if (CollectionUtils.isNotEmpty(basicMatchingEnvironmentVoList)) {
             for (BasicMatchingEnvironmentVo oo : basicMatchingEnvironmentVoList) {
                 if (Objects.equal(new Integer(oo.getType()), baseDataDic.getId())) {
-                    if (StringUtils.isNotBlank(oo.getHumanImpact())) {
-                        builder.append(oo.getCategoryName()).append(oo.getHumanImpact());
+                    if (StringUtils.isNotBlank(oo.getRemark())) {
+                        builder.append(oo.getRemark());
+                    } else if (StringUtils.isNotBlank(oo.getHumanImpact())) {
+                        String name = baseDataDicService.getNameById(oo.getHumanImpact());
+                        if ("无".equals(name)) {
+                            builder.append(oo.getHumanImpact()).append(oo.getCategoryName());
+                        } else {
+                            builder.append(oo.getCategoryName()).append(oo.getHumanImpact());
+                        }
                     }
                     if (StringUtils.isNotBlank(builder.toString())) {
                         hashSet.add(builder.toString());
