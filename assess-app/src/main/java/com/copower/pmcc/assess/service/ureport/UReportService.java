903195f6f9b07eded28704903027227f2a5fbf60
wangpc <372083418@qq.com>
2019-08-02 19:06:56 +0800

    获取开票信息

--- assess-app/src/main/java/com/copower/pmcc/assess/service/ureport/UReportService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/ureport/UReportService.java
@@ -2,8 +2,13 @@ package com.copower.pmcc.assess.service.ureport;
 
 import com.copower.pmcc.assess.dto.output.ureport.UProjectFinanceVo;
 import com.copower.pmcc.assess.service.assist.DdlMySqlAssist;
+import com.copower.pmcc.erp.common.utils.FormatUtils;
+import com.copower.pmcc.erp.common.utils.LangUtils;
+import com.copower.pmcc.finance.api.dto.FinancialBillMakeOutProjectDto;
+import com.copower.pmcc.finance.api.provider.FinanceRpcToolService;
 import com.google.common.collect.Lists;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +25,8 @@ public class UReportService {
     private final Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
     private DdlMySqlAssist ddlMySqlAssist;
+    @Autowired
+    private FinanceRpcToolService financeRpcToolService;
 
     /**
      * 项目开票收款报表
@@ -39,7 +46,7 @@ public class UReportService {
             queryTitle = (String) maps.get("queryTitle");
         }
         StringBuilder sql = new StringBuilder();
-        sql.append("SELECT A.id,A.project_name,A.contract_name,A.contract_price,B.user_account_manager,C.cs_entrustment_unit,C.cs_name,D.u_use_unit," +
+        sql.append("SELECT A.id,A.public_project_id,A.project_name,A.contract_name,A.contract_price,B.user_account_manager,C.cs_entrustment_unit,C.cs_name,D.u_use_unit," +
                 " E.number_value,E.gmt_created" +
                 " FROM tb_project_info A " +
                 " LEFT JOIN tb_project_member B ON A.id=B.project_id" +
@@ -49,20 +56,34 @@ public class UReportService {
 
         List<Map> mapList = ddlMySqlAssist.customTableSelect(sql.toString());
         if (CollectionUtils.isEmpty(mapList)) return null;
+        List<Integer> publicProjectIds = LangUtils.transform(mapList, o -> {
+            return Integer.valueOf(String.valueOf(o.get("public_project_id")));
+        });
+        List<FinancialBillMakeOutProjectDto> makeOutList = financeRpcToolService.getProjectBillMakeOutList(publicProjectIds);
+        if(CollectionUtils.isNotEmpty(makeOutList)){
+            Map<Integer, FinancialBillMakeOutProjectDto> entity = FormatUtils.mappingSingleEntity(makeOutList, o -> o.getProjectId());
+        }
         List<UProjectFinanceVo> list = Lists.newArrayList();
         for (Map map : mapList) {
             UProjectFinanceVo vo = new UProjectFinanceVo();
             vo.setId(Integer.valueOf(String.valueOf(map.get("id"))));
-            vo.setProjectName(String.valueOf(map.get("project_name")));
-            vo.setProjectManagerName(String.valueOf(map.get("user_account_manager")));
-            vo.setConsignorName(String.valueOf(map.get("cs_entrustment_unit")));
-            vo.setReportUseUnitName(String.valueOf(map.get("u_use_unit")));
-            vo.setReportNumber(String.valueOf(map.get("number_value")));
+            vo.setProjectName(objectToString(map.get("project_name")));
+            vo.setProjectManagerName(objectToString(map.get("user_account_manager")));
+            vo.setConsignorName(objectToString(map.get("cs_entrustment_unit")));
+            vo.setReportUseUnitName(objectToString(map.get("u_use_unit")));
+            vo.setReportNumber(objectToString(map.get("number_value")));
            // vo.setReportNumberCreated(DateUtils.convertDate(String.valueOf(map.get("gmt_created"))));
-            vo.setContractName(String.valueOf(map.get("contract_name")));
-            vo.setContractPrice(String.valueOf(map.get("contract_price")));
+            vo.setContractName(objectToString(map.get("contract_name")));
+            vo.setContractPrice(objectToString(map.get("contract_price")));
+
+
             list.add(vo);
         }
         return list;
+    }
+
+    private String objectToString(Object obj){
+        if(obj==null)return "";
+        return StringUtils.defaultString(String.valueOf(obj));
     }
 }
