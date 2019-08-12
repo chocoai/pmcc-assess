1b17a09b9f125f602a3d6fb8ccb7987e0f657941
huhao <1043712950@qq.com>
2019-08-12 10:37:23 +0800

    后续事项监听器

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/event/project/ProjectSubsequentServiceEvent.java
@@ -0,0 +1,25 @@
+package com.copower.pmcc.assess.service.event.project;
+
+import com.copower.pmcc.assess.dal.basis.entity.ProjectSubsequent;
+import com.copower.pmcc.assess.service.ProjectSubsequentService;
+import com.copower.pmcc.assess.service.event.BaseProcessEvent;
+import com.copower.pmcc.bpm.api.dto.model.ProcessExecution;
+import com.copower.pmcc.bpm.api.enums.ProcessStatusEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ProjectSubsequentServiceEvent extends BaseProcessEvent {
+    @Autowired
+    private ProjectSubsequentService projectSubsequentService;
+
+    @Override
+    public void processFinishExecute(ProcessExecution processExecution) throws Exception {
+        super.processFinishExecute(processExecution);
+        ProjectSubsequent data = projectSubsequentService.getDataByProcessInsId(processExecution.getProcessInstanceId());
+        data.setStatus(ProcessStatusEnum.FINISH.getValue());
+        projectSubsequentService.editData(data);
+
+    }
+}
