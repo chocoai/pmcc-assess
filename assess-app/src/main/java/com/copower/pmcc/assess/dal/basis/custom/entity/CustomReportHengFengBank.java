19dfa1735cd803f3597e00548b4966317a111eb8
huhao <1043712950@qq.com>
2020-01-14 11:48:11 +0800

    交通银行报表

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/basis/custom/entity/CustomReportHengFengBank.java
@@ -0,0 +1,253 @@
+package com.copower.pmcc.assess.dal.basis.custom.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * Created by kings on 2018-7-16.
+ */
+public class CustomReportHengFengBank {
+    private Integer id;
+    private Integer projectId;
+    private Integer reportType;//报告类型
+    private String month;//月份
+    private String consignor;//委托人(委托企业名称)
+    private String unitName;//报告使用单位(支行)
+    private String numberValue;//报告编号
+    private String projectName;//项目名称
+    private String workDate;//工作日期
+    private Integer projectCategoryId;
+    private String projectCategoryName;//抵押物性质
+    private String seat;//抵押物地址
+    private BigDecimal area;//面积
+    private BigDecimal assessTotal;//评估金额
+    private String methodNames;//评估方法
+    private String appraiser;//签字估价师
+    private String loan;//贷款金额
+    private String pledgeRatio;//抵押率
+    private String chargePrice;//收费金额
+    private String assessCompany;//评估公司
+    private String remark;//备注
+    private String qualificationType;//资质类型
+    private String realEstateAppraiser;//估价师
+    private Integer areaId;//
+    private Date reportIssuanceDate;
+    private Date homeWorkEndTime;
+    private Date valuationDate;
+
+    public Date getValuationDate() {
+        return valuationDate;
+    }
+
+    public void setValuationDate(Date valuationDate) {
+        this.valuationDate = valuationDate;
+    }
+
+    public Date getReportIssuanceDate() {
+        return reportIssuanceDate;
+    }
+
+    public void setReportIssuanceDate(Date reportIssuanceDate) {
+        this.reportIssuanceDate = reportIssuanceDate;
+    }
+
+    public Date getHomeWorkEndTime() {
+        return homeWorkEndTime;
+    }
+
+    public void setHomeWorkEndTime(Date homeWorkEndTime) {
+        this.homeWorkEndTime = homeWorkEndTime;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    }
+
+    public Integer getReportType() {
+        return reportType;
+    }
+
+    public void setReportType(Integer reportType) {
+        this.reportType = reportType;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getConsignor() {
+        return consignor;
+    }
+
+    public void setConsignor(String consignor) {
+        this.consignor = consignor;
+    }
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+    public String getNumberValue() {
+        return numberValue;
+    }
+
+    public void setNumberValue(String numberValue) {
+        this.numberValue = numberValue;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getWorkDate() {
+        return workDate;
+    }
+
+    public void setWorkDate(String workDate) {
+        this.workDate = workDate;
+    }
+
+    public Integer getProjectCategoryId() {
+        return projectCategoryId;
+    }
+
+    public void setProjectCategoryId(Integer projectCategoryId) {
+        this.projectCategoryId = projectCategoryId;
+    }
+
+    public String getProjectCategoryName() {
+        return projectCategoryName;
+    }
+
+    public void setProjectCategoryName(String projectCategoryName) {
+        this.projectCategoryName = projectCategoryName;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public BigDecimal getArea() {
+        return area;
+    }
+
+    public void setArea(BigDecimal area) {
+        this.area = area;
+    }
+
+    public BigDecimal getAssessTotal() {
+        return assessTotal;
+    }
+
+    public void setAssessTotal(BigDecimal assessTotal) {
+        this.assessTotal = assessTotal;
+    }
+
+    public String getMethodNames() {
+        return methodNames;
+    }
+
+    public void setMethodNames(String methodNames) {
+        this.methodNames = methodNames;
+    }
+
+    public String getAppraiser() {
+        return appraiser;
+    }
+
+    public void setAppraiser(String appraiser) {
+        this.appraiser = appraiser;
+    }
+
+    public String getLoan() {
+        return loan;
+    }
+
+    public void setLoan(String loan) {
+        this.loan = loan;
+    }
+
+    public String getPledgeRatio() {
+        return pledgeRatio;
+    }
+
+    public void setPledgeRatio(String pledgeRatio) {
+        this.pledgeRatio = pledgeRatio;
+    }
+
+    public String getChargePrice() {
+        return chargePrice;
+    }
+
+    public void setChargePrice(String chargePrice) {
+        this.chargePrice = chargePrice;
+    }
+
+    public String getAssessCompany() {
+        return assessCompany;
+    }
+
+    public void setAssessCompany(String assessCompany) {
+        this.assessCompany = assessCompany;
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+    public String getQualificationType() {
+        return qualificationType;
+    }
+
+    public void setQualificationType(String qualificationType) {
+        this.qualificationType = qualificationType;
+    }
+
+    public String getRealEstateAppraiser() {
+        return realEstateAppraiser;
+    }
+
+    public void setRealEstateAppraiser(String realEstateAppraiser) {
+        this.realEstateAppraiser = realEstateAppraiser;
+    }
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+}
---
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/report/CustomReportHengFengBankService.java
@@ -0,0 +1,227 @@
+package com.copower.pmcc.assess.service.report;
+
+import com.copower.pmcc.ad.api.provider.AdRpcQualificationsService;
+import com.copower.pmcc.assess.dal.basis.custom.entity.CustomReportHengFengBank;
+import com.copower.pmcc.assess.dal.basis.custom.mapper.CustomReportHengFengBankMapper;
+import com.copower.pmcc.assess.dal.basis.entity.SchemeJudgeFunction;
+import com.copower.pmcc.assess.dal.basis.entity.SchemeJudgeObject;
+import com.copower.pmcc.assess.dto.output.project.initiate.InitiateConsignorVo;
+import com.copower.pmcc.assess.service.BaseService;
+import com.copower.pmcc.assess.service.PublicService;
+import com.copower.pmcc.assess.service.base.BaseDataDicService;
+import com.copower.pmcc.assess.service.base.BaseProjectClassifyService;
+import com.copower.pmcc.assess.service.project.initiate.InitiateConsignorService;
+import com.copower.pmcc.assess.service.project.initiate.InitiatePossessorService;
+import com.copower.pmcc.assess.service.project.scheme.SchemeJudgeFunctionService;
+import com.copower.pmcc.assess.service.project.scheme.SchemeJudgeObjectService;
+import com.copower.pmcc.assess.service.project.survey.SurveyCommonService;
+import com.copower.pmcc.erp.api.dto.model.BootstrapTableVo;
+import com.copower.pmcc.erp.common.exception.BusinessException;
+import com.copower.pmcc.erp.common.support.mvc.request.RequestBaseParam;
+import com.copower.pmcc.erp.common.support.mvc.request.RequestContext;
+import com.copower.pmcc.erp.common.utils.DateUtils;
+import com.copower.pmcc.erp.common.utils.FormatUtils;
+import com.copower.pmcc.erp.common.utils.LangUtils;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomReportHengFengBankService {
+    @Autowired
+    private BaseDataDicService baseDataDicService;
+    @Autowired
+    private PublicService publicService;
+    @Autowired
+    private SchemeJudgeObjectService schemeJudgeObjectService;
+    @Autowired
+    private InitiateConsignorService initiateConsignorService;
+    @Autowired
+    private AdRpcQualificationsService adRpcQualificationsService;
+    @Autowired
+    private BaseService baseService;
+    @Autowired
+    private CustomReportHengFengBankMapper customReportHengFengBankMapper;
+    @Autowired
+    private BaseProjectClassifyService baseProjectClassifyService;
+    @Autowired
+    private InitiatePossessorService initiatePossessorService;
+    @Autowired
+    private SurveyCommonService surveyCommonService;
+    @Autowired
+    private SchemeJudgeFunctionService schemeJudgeFunctionService;
+
+
+    public BootstrapTableVo getCustomReportHengFengBankList(String numberValue, String unitName, Integer reportType) {
+        BootstrapTableVo vo = new BootstrapTableVo();
+        RequestBaseParam requestBaseParam = RequestContext.getRequestBaseParam();
+        Page<PageInfo> page = PageHelper.startPage(requestBaseParam.getOffset(), requestBaseParam.getLimit());
+        List<CustomReportHengFengBank> customNumberRecordList = customReportHengFengBankMapper.getCustomReportHengFengBankList(numberValue, unitName, reportType);
+        List<CustomReportHengFengBank> vos = LangUtils.transform(customNumberRecordList, o -> getCustomReportHengFengBank(o));
+        vo.setTotal(page.getTotal());
+        vo.setRows(CollectionUtils.isEmpty(vos) ? new ArrayList<CustomReportHengFengBank>() : vos);
+        return vo;
+    }
+
+    public CustomReportHengFengBank getCustomReportHengFengBank(CustomReportHengFengBank data) {
+        if (data == null) {
+            return null;
+        }
+        CustomReportHengFengBank vo = new CustomReportHengFengBank();
+        BeanUtils.copyProperties(data, vo);
+        //月份
+        if (data.getValuationDate() != null) {
+            vo.setMonth(String.format("%s%s", DateUtils.getMonth(data.getValuationDate()), "月"));
+        }
+        //工作日期
+        if (data.getReportIssuanceDate() != null && data.getHomeWorkEndTime() != null) {
+            vo.setWorkDate(String.format("%s~%s", DateUtils.format(data.getReportIssuanceDate(), DateUtils.DATE_PATTERN), DateUtils.format(data.getHomeWorkEndTime(), DateUtils.DATE_PATTERN)));
+        }
+        //抵押物性质
+        vo.setProjectCategoryName(baseProjectClassifyService.getNameById(data.getProjectCategoryId()));
+        if (data.getAreaId() != null && data.getAreaId() != 0) {
+            List<SchemeJudgeFunction> functionList = schemeJudgeFunctionService.getApplicableJudgeFunctionsByAreaId(data.getAreaId());
+            List<String> methods = LangUtils.transform(functionList, o -> o.getName());
+            //去重
+            String districtmethods = publicService.districtString(methods);
+            //获取方法
+            vo.setMethodNames(districtmethods);
+            List<SchemeJudgeObject> judgeObjectList = schemeJudgeObjectService.getJudgeObjectFullListByAreaId(data.getAreaId());
+            if (CollectionUtils.isNotEmpty(judgeObjectList)) {
+                vo.setArea(judgeObjectList.get(0).getEvaluationArea());
+                //评估总价
+                vo.setAssessTotal(judgeObjectList.get(0).getEvaluationArea().multiply(judgeObjectList.get(0).getPrice()).setScale(2, BigDecimal.ROUND_HALF_UP));
+                //抵押物地址
+                vo.setSeat(judgeObjectList.get(0).getSeat());
+            }
+        }
+        //委托人
+        InitiateConsignorVo consignorVo = initiateConsignorService.getDataByProjectId(data.getProjectId());
+        String principalStr = StringUtils.isNotBlank(consignorVo.getCsName()) ? consignorVo.getCsName() : consignorVo.getCsEntrustmentUnit();
+        vo.setConsignor(principalStr);
+
+        //评估类型
+        vo.setProjectCategoryName(baseProjectClassifyService.getNameById(data.getProjectCategoryId()));
+        //签字估价师
+        if (StringUtils.isNotEmpty(data.getRealEstateAppraiser())) {
+            List<String> names = FormatUtils.transformString2List(data.getRealEstateAppraiser());
+            try {
+                StringBuilder s = new StringBuilder();
+                for (String account : names) {
+                    s.append(publicService.getUserNameByAccount(account)).append("、");
+                }
+                s.deleteCharAt(s.length() - 1);
+                vo.setAppraiser(s.toString());
+            } catch (Exception e) {
+                baseService.writeExceptionInfo(e, "签字估价师");
+            }
+
+        }
+
+
+        return vo;
+
+    }
+
+    /**
+     * 导出
+     *
+     * @param response
+     */
+    public void export(HttpServletResponse response, String numberValue, String unitName, Integer reportType) throws BusinessException, IOException {
+        //获取数据
+        List<CustomReportHengFengBank> customNumberRecordList = customReportHengFengBankMapper.getCustomReportHengFengBankList(numberValue, unitName, reportType);
+        List<CustomReportHengFengBank> vos = LangUtils.transform(customNumberRecordList, o -> getCustomReportHengFengBank(o));
+
+        if (CollectionUtils.isEmpty(customNumberRecordList)) {
+            throw new BusinessException("没有获取到有效的数据");
+        }
+        Workbook wb = new HSSFWorkbook();
+        Sheet sheet = wb.createSheet();
+        Row row = sheet.createRow(0);
+        //创建Excel标题
+        String[] title = {"月份", "委托方", "上报部门", "报告编号", "项目名称",
+                "工作日期", "抵押物性质", "抵押物地址",
+                "面积", "评估金额", "评估方法", "签字估价师",
+                "贷款金额", "抵押率", "收费金额", "评估公司", "备注"};
+
+        for (int i = 0; i < title.length; i++) {
+            Cell cell = row.createCell(i);
+            cell.setCellValue(title[i]);
+            sheet.setColumnWidth(i, 4000);
+        }
+
+        for (int i = 0; i < vos.size(); i++) {
+            row = sheet.createRow(i + 1);
+            CustomReportHengFengBank association = vos.get(i);
+            row.createCell(0).setCellValue(association.getMonth()); // 月份
+            row.createCell(1).setCellValue(association.getConsignor()); // 委托人
+            row.createCell(2).setCellValue(association.getUnitName()); // 报告使用单位
+            row.createCell(3).setCellValue(association.getNumberValue()); // 报告编号
+            row.createCell(4).setCellValue(association.getProjectName()); // 项目名称
+            row.createCell(5).setCellValue(association.getWorkDate()); // 工作日期
+            row.createCell(6).setCellValue(association.getProjectCategoryName()); // 抵押物性质
+            row.createCell(7).setCellValue(association.getSeat()); // 抵押物地址
+            row.createCell(8).setCellValue(String.valueOf(association.getArea() == null ? "" : association.getArea())); // 面积
+            row.createCell(9).setCellValue(String.valueOf(association.getAssessTotal() == null ? "" : association.getAssessTotal())); // 评估金额
+            row.createCell(10).setCellValue(association.getMethodNames()); // 评估方法
+            row.createCell(11).setCellValue(association.getAppraiser()); // 评估方法
+            row.createCell(12).setCellValue(String.valueOf(association.getLoan() == null ? "" : association.getLoan())); // 贷款金额
+            row.createCell(13).setCellValue(association.getPledgeRatio()); // 抵押率
+            row.createCell(14).setCellValue(String.valueOf(association.getChargePrice() == null ? "" : association.getChargePrice())); // 收费金额
+            row.createCell(15).setCellValue(association.getAssessCompany()); // 评估公司
+            row.createCell(16).setCellValue(association.getRemark()); // 备注
+        }
+
+        OutputStream os = response.getOutputStream();
+        try {
+            this.setResponseHeader(response, "恒丰银行.XLS");
+            wb.write(os);
+        } catch (Exception e) {
+            throw new BusinessException("导出Excel出错:" + e);
+        } finally {
+            os.flush();
+            os.close();
+        }
+    }
+
+    /**
+     * 响应流
+     *
+     * @param response
+     * @param fileName
+     */
+    public void setResponseHeader(HttpServletResponse response, String fileName) {
+        try {
+            try {
+                fileName = new String(fileName.getBytes(), "ISO8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+            response.setContentType("application/octet-stream;charset=ISO8859-1");
+            response.setHeader("Content-Disposition", "attachment;filename=" + fileName);
+            response.addHeader("Pargam", "no-cache");
+            response.addHeader("Cache-Control", "no-cache");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
