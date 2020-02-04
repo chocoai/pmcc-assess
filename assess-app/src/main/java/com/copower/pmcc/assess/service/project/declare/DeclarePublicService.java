ad5446c221d05f2a64245883a25c3f51319da477
zch <noatnu@163.com>
2020-02-04 16:14:11 +0800

    删除一个冗余方法(判断是否是数字的方法)

--- assess-app/src/main/java/com/copower/pmcc/assess/service/project/declare/DeclarePublicService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/project/declare/DeclarePublicService.java
@@ -844,20 +844,6 @@ public class DeclarePublicService {
         return declareApplyDao.getDeclareApplyByProjectId(projectId);
     }
 
-    //验证是否是数字或小数
-    public boolean isNumeric(String str) {
-        Pattern pattern = Pattern.compile("[0-9]*");
-        if (str.indexOf(".") > 0) {//判断是否有小数点
-            if (str.indexOf(".") == str.lastIndexOf(".") && str.split("\\.").length == 2) { //判断是否只有一个小数点
-                return pattern.matcher(str.replace(".", "")).matches();
-            } else {
-                return false;
-            }
-        } else {
-            return pattern.matcher(str).matches();
-        }
-
-    }
 
     /**
      * 申报图片自动关联
