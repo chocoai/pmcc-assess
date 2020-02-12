9a170b4f6249d10899263a8424c99e6be35865e1
zch <noatnu@163.com>
2020-02-12 18:39:41 +0800

    考核数据多维度分析报表 临时提交

---
+++ assess-app/src/main/java/com/copower/pmcc/assess/controller/chks/ChksMultidimensionalReportController.java
@@ -0,0 +1,16 @@
+package com.copower.pmcc.assess.controller.chks;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zch on 2020-2-12.
+ * 考核数据多维度分析报表
+ */
+
+
+@RestController
+@RequestMapping(value = "/chksMultidimensionalReport")
+public class ChksMultidimensionalReportController {
+
+}
