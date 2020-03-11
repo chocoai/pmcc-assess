24b6c6932c52087af00749efb78712b78750ad94
wangpc <372083418@qq.com>
2020-03-11 09:02:50 +0800

    审批显示出跳多级

--- assess-app/src/main/java/com/copower/pmcc/assess/service/event/EmptyProcessEvent.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/event/EmptyProcessEvent.java
@@ -2,11 +2,13 @@ package com.copower.pmcc.assess.service.event;
 
 import com.copower.pmcc.bpm.api.dto.model.ProcessExecution;
 import com.copower.pmcc.bpm.api.executor.ProcessEventExecutor;
+import org.springframework.stereotype.Component;
 
 /**
  * 空监听器
  * Created by wangpc on 2019-10-22.
  */
+@Component
 public class EmptyProcessEvent implements ProcessEventExecutor {
     @Override
     public void processStartExecute(String processInstanceId) throws Exception {
