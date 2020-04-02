6ecc848769393c96ca35074071ec4fb3374627c1
wangpc <372083418@qq.com>
2020-04-02 11:30:13 +0800

    调整报告文号处理

--- assess-app/src/main/java/com/copower/pmcc/assess/service/event/project/ProjectTakeNumberServiceEvent.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/event/project/ProjectTakeNumberServiceEvent.java
@@ -10,8 +10,11 @@ import com.copower.pmcc.assess.service.project.ProjectNumberRecordService;
 import com.copower.pmcc.bpm.api.dto.model.ProcessExecution;
 import com.copower.pmcc.bpm.api.enums.ProcessStatusEnum;
 import com.copower.pmcc.erp.api.dto.SysSymbolListDto;
+import com.copower.pmcc.erp.api.provider.ErpRpcToolsService;
+import com.copower.pmcc.erp.constant.ApplicationConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Component
@@ -22,14 +25,20 @@ public class ProjectTakeNumberServiceEvent extends BaseProcessEvent {
     private ProjectInfoService projectInfoService;
     @Autowired
     private ProjectNumberRecordService projectNumberRecordService;
+    @Autowired
+    private ApplicationConstant applicationConstant;
+    @Autowired
+    private ErpRpcToolsService erpRpcToolsService;
 
     @Override
+    @Transactional(rollbackFor = Exception.class)
     public void processFinishExecute(ProcessExecution processExecution) throws Exception {
         ProjectTakeNumber projectTakeNumber = projectTakeNumberService.getDataByProcessInsId(processExecution.getProcessInstanceId());
         projectTakeNumber.setStatus(ProcessStatusEnum.FINISH.getValue());
         ProjectInfo projectInfo = projectInfoService.getProjectInfoById(projectTakeNumber.getProjectId());
         AssessProjectTypeEnum assessProjectType = projectInfoService.getAssessProjectType(projectInfo.getProjectCategoryId());
         SysSymbolListDto symbolListDto = projectNumberRecordService.getReportNumber(projectInfo, 0, assessProjectType, projectTakeNumber.getReportType(), true);
+        erpRpcToolsService.updateSymbolUsed(applicationConstant.getAppKey(), symbolListDto.getSymbol());
         projectTakeNumber.setNumberValue(symbolListDto.getSymbol());
         projectTakeNumber.setAssessProjectType(assessProjectType.getKey());
         projectTakeNumberService.editData(projectTakeNumber);
