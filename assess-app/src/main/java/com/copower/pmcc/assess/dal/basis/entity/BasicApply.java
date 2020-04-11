8c500441f19ad5e294d76f3e7760db5c3d06a5cc
wangpc <372083418@qq.com>
2020-04-11 16:06:05 +0800

    去除多余字段

--- assess-app/src/main/java/com/copower/pmcc/assess/dal/basis/entity/BasicApply.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/dal/basis/entity/BasicApply.java
@@ -12,15 +12,9 @@ public class BasicApply {
 
     private String structuralInfo;
 
+    private Integer type;
+
     private String address;
-
-    private Integer caseEstateId;
-
-    private Integer caseBuildingId;
-
-    private Integer caseUnitId;
-
-    private Integer caseHouseId;
 
     private Integer declareRecordId;
 
@@ -35,38 +29,6 @@ public class BasicApply {
     private Integer planDetailsId;
 
     private BigDecimal area;
-
-    private String processInsId;
-
-    private String province;
-
-    private String city;
-
-    private Integer type;
-
-    private String estateName;
-
-    private String buildingNumber;
-
-    private String unitNumber;
-
-    private String houseNumber;
-
-    private String status;
-
-    private Boolean draftFlag;
-
-    private String estatePartInMode;
-
-    private String buildingPartInMode;
-
-    private String unitPartInMode;
-
-    private String housePartInMode;
-
-    private Boolean writeBackBlockFlag;
-
-    private Integer copyId;
 
     private Boolean bisDelete;
 
@@ -108,44 +70,20 @@ public class BasicApply {
         this.structuralInfo = structuralInfo == null ? null : structuralInfo.trim();
     }
 
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+
     public String getAddress() {
         return address;
     }
 
     public void setAddress(String address) {
         this.address = address == null ? null : address.trim();
-    }
-
-    public Integer getCaseEstateId() {
-        return caseEstateId;
-    }
-
-    public void setCaseEstateId(Integer caseEstateId) {
-        this.caseEstateId = caseEstateId;
-    }
-
-    public Integer getCaseBuildingId() {
-        return caseBuildingId;
-    }
-
-    public void setCaseBuildingId(Integer caseBuildingId) {
-        this.caseBuildingId = caseBuildingId;
-    }
-
-    public Integer getCaseUnitId() {
-        return caseUnitId;
-    }
-
-    public void setCaseUnitId(Integer caseUnitId) {
-        this.caseUnitId = caseUnitId;
-    }
-
-    public Integer getCaseHouseId() {
-        return caseHouseId;
-    }
-
-    public void setCaseHouseId(Integer caseHouseId) {
-        this.caseHouseId = caseHouseId;
     }
 
     public Integer getDeclareRecordId() {
@@ -202,134 +140,6 @@ public class BasicApply {
 
     public void setArea(BigDecimal area) {
         this.area = area;
-    }
-
-    public String getProcessInsId() {
-        return processInsId;
-    }
-
-    public void setProcessInsId(String processInsId) {
-        this.processInsId = processInsId == null ? null : processInsId.trim();
-    }
-
-    public String getProvince() {
-        return province;
-    }
-
-    public void setProvince(String province) {
-        this.province = province == null ? null : province.trim();
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city == null ? null : city.trim();
-    }
-
-    public Integer getType() {
-        return type;
-    }
-
-    public void setType(Integer type) {
-        this.type = type;
-    }
-
-    public String getEstateName() {
-        return estateName;
-    }
-
-    public void setEstateName(String estateName) {
-        this.estateName = estateName == null ? null : estateName.trim();
-    }
-
-    public String getBuildingNumber() {
-        return buildingNumber;
-    }
-
-    public void setBuildingNumber(String buildingNumber) {
-        this.buildingNumber = buildingNumber == null ? null : buildingNumber.trim();
-    }
-
-    public String getUnitNumber() {
-        return unitNumber;
-    }
-
-    public void setUnitNumber(String unitNumber) {
-        this.unitNumber = unitNumber == null ? null : unitNumber.trim();
-    }
-
-    public String getHouseNumber() {
-        return houseNumber;
-    }
-
-    public void setHouseNumber(String houseNumber) {
-        this.houseNumber = houseNumber == null ? null : houseNumber.trim();
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status == null ? null : status.trim();
-    }
-
-    public Boolean getDraftFlag() {
-        return draftFlag;
-    }
-
-    public void setDraftFlag(Boolean draftFlag) {
-        this.draftFlag = draftFlag;
-    }
-
-    public String getEstatePartInMode() {
-        return estatePartInMode;
-    }
-
-    public void setEstatePartInMode(String estatePartInMode) {
-        this.estatePartInMode = estatePartInMode == null ? null : estatePartInMode.trim();
-    }
-
-    public String getBuildingPartInMode() {
-        return buildingPartInMode;
-    }
-
-    public void setBuildingPartInMode(String buildingPartInMode) {
-        this.buildingPartInMode = buildingPartInMode == null ? null : buildingPartInMode.trim();
-    }
-
-    public String getUnitPartInMode() {
-        return unitPartInMode;
-    }
-
-    public void setUnitPartInMode(String unitPartInMode) {
-        this.unitPartInMode = unitPartInMode == null ? null : unitPartInMode.trim();
-    }
-
-    public String getHousePartInMode() {
-        return housePartInMode;
-    }
-
-    public void setHousePartInMode(String housePartInMode) {
-        this.housePartInMode = housePartInMode == null ? null : housePartInMode.trim();
-    }
-
-    public Boolean getWriteBackBlockFlag() {
-        return writeBackBlockFlag;
-    }
-
-    public void setWriteBackBlockFlag(Boolean writeBackBlockFlag) {
-        this.writeBackBlockFlag = writeBackBlockFlag;
-    }
-
-    public Integer getCopyId() {
-        return copyId;
-    }
-
-    public void setCopyId(Integer copyId) {
-        this.copyId = copyId;
     }
 
     public Boolean getBisDelete() {
--- assess-app/src/main/java/com/copower/pmcc/assess/service/data/DataBlockService.java
+++ assess-app/src/main/java/com/copower/pmcc/assess/service/data/DataBlockService.java
@@ -196,52 +196,56 @@ public class DataBlockService {
         List<BasicHouse> basicHouseList = basicHouseService.getBasicHouseList(new BasicHouse());
         if (CollectionUtils.isNotEmpty(basicHouseList)) {
             for (BasicHouse basicHouse : basicHouseList) {
-                if (key == 3) {
-                    List<BasicHouseRoom> basicHouseRoomList = basicHouseRoomService.getBasicHouseRoomList(basicHouse.getId());
-                    if (CollectionUtils.isNotEmpty(basicHouseRoomList)) {
-                        for (BasicHouseRoom basicHouseRoom : basicHouseRoomList) {
-                            BasicHouseRoomDecorate where = new BasicHouseRoomDecorate();
-                            where.setRoomId(basicHouseRoom.getId());
-                            List<BasicHouseRoomDecorate> houseRoomDecorateList = basicHouseRoomDecorateService.basicHouseRoomDecorateList(where);
-                            if (CollectionUtils.isNotEmpty(houseRoomDecorateList)) {
-                                for (BasicHouseRoomDecorate basicHouseRoomDecorate : houseRoomDecorateList) {
-                                    basicHouseRoomDecorate.setHouseId(basicHouse.getId());
-                                    basicHouseRoomDecorateService.saveAndUpdateBasicHouseRoomDecorate(basicHouseRoomDecorate, false);
+                try{
+                    if (key == 3) {
+                        List<BasicHouseRoom> basicHouseRoomList = basicHouseRoomService.getBasicHouseRoomList(basicHouse.getId());
+                        if (CollectionUtils.isNotEmpty(basicHouseRoomList)) {
+                            for (BasicHouseRoom basicHouseRoom : basicHouseRoomList) {
+                                BasicHouseRoomDecorate where = new BasicHouseRoomDecorate();
+                                where.setRoomId(basicHouseRoom.getId());
+                                List<BasicHouseRoomDecorate> houseRoomDecorateList = basicHouseRoomDecorateService.basicHouseRoomDecorateList(where);
+                                if (CollectionUtils.isNotEmpty(houseRoomDecorateList)) {
+                                    for (BasicHouseRoomDecorate basicHouseRoomDecorate : houseRoomDecorateList) {
+                                        basicHouseRoomDecorate.setHouseId(basicHouse.getId());
+                                        basicHouseRoomDecorateService.saveAndUpdateBasicHouseRoomDecorate(basicHouseRoomDecorate, false);
+                                    }
                                 }
                             }
                         }
                     }
-                }
-                if(key==4){
-                    BasicApply where = new BasicApply();
-                    where.setBasicHouseId(basicHouse.getId());
-                    List<BasicApply> basicApplies = basicApplyDao.getBasicApplyList(where);
-                    if (CollectionUtils.isNotEmpty(basicApplies)) {
-                        BasicApply apply = basicApplies.get(0);
-                        if(StringUtils.isNotBlank(apply.getStructuralInfo()) ){
-                            BasicApplyBatchDetail basicApplyBatchDetail = basicApplyBatchDetailService.getBasicApplyBatchDetail(FormatUtils.entityNameConvertToTableName(BasicHouse.class), basicHouse.getId());
-                            ProjectPlanDetails projectPlanDetails = projectPlanDetailsService.getProjectPlanDetailsById(apply.getPlanDetailsId());
-                            if (projectPlanDetails != null)
-                                apply.setDeclareRecordId(projectPlanDetails.getDeclareRecordId());
-                            if(basicApplyBatchDetail!=null){
-                                apply.setBatchDetailId(basicApplyBatchDetail.getId());
-                                List<BasicApplyBatchDetail> list = Lists.newArrayList();
-                                basicApplyBatchDetailService.collectionParentBatchDetails(basicApplyBatchDetail.getId(), list);
-                                List<KeyValueDto> keyValueDtos = Lists.newArrayList();
-                                for (int i = list.size() - 1; i >= 0; i--) {
-                                    BasicApplyBatchDetail batchDetail = list.get(i);
-                                    KeyValueDto keyValueDto = new KeyValueDto();
-                                    keyValueDto.setKey(batchDetail.getType());
-                                    keyValueDto.setValue(String.valueOf(batchDetail.getTableId()));
-                                    keyValueDtos.add(keyValueDto);
+                    if(key==4){
+                        BasicApply where = new BasicApply();
+                        where.setBasicHouseId(basicHouse.getId());
+                        List<BasicApply> basicApplies = basicApplyDao.getBasicApplyList(where);
+                        if (CollectionUtils.isNotEmpty(basicApplies)) {
+                            BasicApply apply = basicApplies.get(0);
+                            if(StringUtils.isNotBlank(apply.getStructuralInfo()) ){
+                                BasicApplyBatchDetail basicApplyBatchDetail = basicApplyBatchDetailService.getBasicApplyBatchDetail(FormatUtils.entityNameConvertToTableName(BasicHouse.class), basicHouse.getId());
+                                ProjectPlanDetails projectPlanDetails = projectPlanDetailsService.getProjectPlanDetailsById(apply.getPlanDetailsId());
+                                if (projectPlanDetails != null)
+                                    apply.setDeclareRecordId(projectPlanDetails.getDeclareRecordId());
+                                if(basicApplyBatchDetail!=null){
+                                    apply.setBatchDetailId(basicApplyBatchDetail.getId());
+                                    List<BasicApplyBatchDetail> list = Lists.newArrayList();
+                                    basicApplyBatchDetailService.collectionParentBatchDetails(basicApplyBatchDetail.getId(), list);
+                                    List<KeyValueDto> keyValueDtos = Lists.newArrayList();
+                                    for (int i = list.size() - 1; i >= 0; i--) {
+                                        BasicApplyBatchDetail batchDetail = list.get(i);
+                                        KeyValueDto keyValueDto = new KeyValueDto();
+                                        keyValueDto.setKey(batchDetail.getType());
+                                        keyValueDto.setValue(String.valueOf(batchDetail.getTableId()));
+                                        keyValueDtos.add(keyValueDto);
+                                    }
+                                    apply.setStructuralInfo(JSON.toJSONString(keyValueDtos));
+                                    basicApplyBatchDetail.setDeclareRecordId(projectPlanDetails.getDeclareRecordId());
+                                    basicApplyBatchDetailService.saveBasicApplyBatchDetail(basicApplyBatchDetail);
                                 }
-                                apply.setStructuralInfo(JSON.toJSONString(keyValueDtos));
-                                basicApplyBatchDetail.setDeclareRecordId(projectPlanDetails.getDeclareRecordId());
-                                basicApplyBatchDetailService.saveBasicApplyBatchDetail(basicApplyBatchDetail);
+                                basicApplyDao.updateBasicApply(apply);
                             }
-                            basicApplyDao.updateBasicApply(apply);
                         }
                     }
+                }catch (Exception ex){
+                    logger.error(ex.getMessage(),ex);
                 }
             }
         }
